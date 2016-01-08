f29d915ed65a3e6df7102409cd2134112ce2328c
QarthO <QarthO@gmail.com>
2016-01-08 17:56:45 -0500

    sort script.js
    Cleaned up the code a little bit

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
-  function getSummonerId(summoner){
-  	var formatsummonername = summoner.toLowerCase().replace(/\W*/g, "");
-  	var summonerid = "test  ";
-  	fetch("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summoner + "?api_key=03e76ce4-0e39-4501-9244-b2194ac146a7")
-  	.then(
-  		function(response) {
-  			if (response.status !== 200) {
-  				console.log('Looks like there was a problem. Status Code: ' +
-  					response.status);
-  				return;
-  			}
-  			response.json().then(function(data) {
-  				console.log(data);
-  				console.log(data[formatsummonername].id);
-  				summonerid = data[formatsummonername].id;
-  				var fs = require("fs");
+const apigetSummonerId = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/";
+const apikey = "api_key=03e76ce4-0e39-4501-9244-b2194ac146a7"
+const fs = require("fs");
 
-  				var profiles = JSON.parse(fs.readFileSync("profiles.json")) || [];
+function getSummonerId(summoner){
 
-  				profiles.push({
-  					"summoner": summoner,
-  					"id": summonerid
-  				});
+  fetch(apigetSummonerId + summoner + "?" + apikey)
+  .then(
 
-  				fs.writeFile("profiles.json", JSON.stringify(profiles));
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+        response.status);
+        return;
+      }
+      response.json().then(function(data) {
 
-  				alert(summoner + "'s ID: " + summonerid);
-  				return summonerid;
-  			});
-  		}
-  		)
-  	.catch(function(err) {
-  		console.log('Fetch Error :-S', err);
-  	});
-  	return summonerid;
-  }
-  function apply(){
-  	var id = getSummonerId(document.getElementById("summoner").value);
-  	var fs = require("fs");
-  	fs.writeFile("Summonor", document.getElementById("summoner").value);
-  }
+        console.log(data);
+        console.log(data[formatsummonername].id);
+
+        summonerid = data[formatsummonername].id;
+
+        var profiles = JSON.parse(fs.readFileSync("profiles.json")) || [];
+
+        profiles.push({
+          "summoner": summoner,
+          "id": summonerid
+        });
+
+        fs.writeFile("profiles.json", JSON.stringify(profiles));
+
+        alert(summoner + "'s ID: " + summonerid);
+
+        return summonerid;
+      });
+    }
+  )
+
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+  return summonerid;
+}
+
+function apply(){
+
+  var id = getSummonerId(document.getElementById("summoner").value);
+
+}
